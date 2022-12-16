a95b9c56b693c7d76f5826ed6fb7119f857696e6
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2022-12-16 08:49:25 -0300

    feat: created Form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,20 @@
+import { PlusCircle } from "phosphor-react";
+
+export function Form() {
+  return (
+    <form className="w-full flex gap-2 -translate-y-6">
+      <input
+        className="flex-1 p-4 bg-gray-500 text-gray-100 rounded-lg placeholder:text-gray-300"
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+      />
+      <button
+        className="p-4 flex items-center gap-2 bg-sky-700 text-gray-100 rounded-lg transition-colors hover:bg-sky-500"
+        type="submit"
+      >
+        Criar
+        <PlusCircle size={16} />
+      </button>
+    </form>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif"
+      },
+      colors: {
+        gray: {
+          100: "#F2F2F2",
+          200: "#D9D9D9",
+          300: "#808080",
+          400: "#333333",
+          500: "#262626",
+          600: "#1A1A1A",
+          700: "#0D0D0D",
+        },
+        sky: {
+          500: "#4EA8DE",
+          700: "#1E6F9F",
+        },
+        red: {
+          500: "#E25858",
+        },
+        indigo: {
+          400: "#8284FA",
+          600: "#5E60CE",
+        }
+      },
+    },
   },
   plugins: [],
 }
